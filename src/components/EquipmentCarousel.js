694da7436b900cf799c13a464e727932ef3c40ed
varungarade1151@gmail.com <your.email@example.com>
2024-11-15 11:57:06 +0530

    images changed in equipment corousal

--- src/components/EquipmentCarousel.js
+++ src/components/EquipmentCarousel.js
@@ -7,9 +7,9 @@ const EquipmentCarousel = () => {
   const settings = { dots: true, infinite: true, speed: 500, slidesToShow: 3, slidesToScroll: 1 };
 
   const equipmentImages = [
-    'https://media.istockphoto.com/id/817585012/photo/construction.jpg?s=1024x1024&w=is&k=20&c=zGSwPUI2g4uuu7lEcGlOl2sJAAafQwm4-o3EbjWojz0=', // Equipment 1
-    'https://plus.unsplash.com/premium_photo-1681690860621-57d749a22f34?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Equipment 2
-    'https://plus.unsplash.com/premium_photo-1681691912442-68c4179c530c?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D '] ;
+    'https://images.unsplash.com/photo-1504328345606-18bbc8c9d7d1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Equipment 1
+    'https://images.unsplash.com/photo-1694522362256-6c907336af43?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Equipment 2
+    'https://plus.unsplash.com/premium_photo-1681989490797-dbe51c438b61?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'] ;
 
   return (
     <section className="py-10 bg-gray-100">
